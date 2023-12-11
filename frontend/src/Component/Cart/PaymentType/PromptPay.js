472487ec298d757490bd5d5bd7a8a7bd866d93d3
monzzzz <elmond.pattanan@gmail.com>
2023-12-10 20:00:50 -0800

    promptpay hook

--- frontend/src/Component/Cart/PaymentType/PromptPay.js
+++ frontend/src/Component/Cart/PaymentType/PromptPay.js
@@ -1,70 +1,26 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Script from "react-load-script";
 import "../../../Assets/style/Cart/PaymentType/PromptPay.css";
 import { CountdownTimer } from "./CountdownExpires";
 import ContentLoader from "react-content-loader";
+import usePromptpay from "../../../Hook/Cart/PaymentType/usePromptpay";
+import { formatPrice } from "../../../Utils/PriceFormat";
 export default function PromptPay({ paymentType, amount }) {
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-  const [data, setData] = useState("");
-
+  const {
+    isLoading,
+    paymentInfo,
+    scriptLoaded,
+    handleFormSubmit,
+    handleScriptLoad,
+  } = usePromptpay(amount, paymentType);
   useEffect(() => {
     if (scriptLoaded) {
       window.Omise.setPublicKey("pkey_test_5xoev7sn4wflkzseb52");
     }
     if (paymentType === "promptpay" && scriptLoaded === true) {
-      handleFormSubmit();
+      handleFormSubmit(amount);
     }
   }, [scriptLoaded]);
-
-  const handleScriptLoad = () => {
-    setScriptLoaded(true);
-  };
-
-  const handleFormSubmit = () => {
-    const paymentData = {
-      amount: amount, // Example amount in satangs
-      currency: "THB",
-    };
-
-    if (window.Omise) {
-      window.Omise.createSource(
-        "promptpay",
-        paymentData,
-        (statusCode, response) => {
-          if (response.object === "source" && response.id) {
-            sendSourceToBackend(response.id, response.amount);
-            console.log(response);
-          } else {
-            console.error(response);
-          }
-        }
-      );
-    } else {
-      console.error("Omise library not loaded");
-    }
-  };
-  const sendSourceToBackend = async (sourceId, amount) => {
-    try {
-      const response = await fetch("/api/payment/promptpay", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ sourceId, amount }), // pass sourceId an attribute
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const json = await response.json();
-      setData(json.data); //json.imageObject.download_uri
-
-      // console.log("Charge created:", data);
-    } catch (error) {
-      console.error("Error sending source to backend:", error);
-    }
-  };
 
   return (
     <div className="promptpay-payment">
@@ -73,26 +29,7 @@ export default function PromptPay({ paymentType, amount }) {
         <div className="promptpay-container">
           <h1 className="mb-5">Payment</h1>
 
-          {data ? (
-            <div className="promptpay-qr-frame d-flex">
-              <div className="qrcode-image-container col-lg-5 col-md-5 col-sm-12">
-                <img
-                  src={data.source.scannable_code.image.download_uri}
-                  alt="promptpay-qrcode"
-                  className="qrcode-image"
-                  style={{ width: "80%" }}
-                />
-              </div>
-              <div className="col-lg-7 col-md-7 col-sm-12 d-flex flex-column">
-                <div className="promptpay-qrcode-amount mb-3">
-                  Amount: {data.source.amount} {data.source.currency}
-                </div>
-                <div className="promptpay-expired-date">
-                  Expired: <CountdownTimer expiryDate={data.expires_at} />
-                </div>
-              </div>
-            </div>
-          ) : (
+          {isLoading ? (
             <div>
               <ContentLoader viewBox="0 0 380 200">
                 <rect x="0" y="0" rx="5" ry="5" width="110" height="150" />
@@ -102,6 +39,28 @@ export default function PromptPay({ paymentType, amount }) {
                 <rect x="325" y="80" rx="3" ry="3" width="50" height="10" />
               </ContentLoader>
             </div>
+          ) : (
+            paymentInfo && (
+              <div className="promptpay-qr-frame d-flex">
+                <div className="qrcode-image-container col-lg-5 col-md-5 col-sm-12">
+                  <img
+                    src={paymentInfo.source.scannable_code.image.download_uri}
+                    alt="promptpay-qrcode"
+                    className="qrcode-image"
+                    style={{ width: "80%" }}
+                  />
+                </div>
+                <div className="col-lg-7 col-md-7 col-sm-12 d-flex flex-column">
+                  <div className="promptpay-qrcode-amount mb-3">
+                    Amount: {formatPrice(paymentInfo.source.amount)}{" "}
+                  </div>
+                  <div className="promptpay-expired-date">
+                    Expired:{" "}
+                    <CountdownTimer expiryDate={paymentInfo.expires_at} />
+                  </div>
+                </div>
+              </div>
+            )
           )}
         </div>
       ) : (
---
+++ frontend/src/Hook/Cart/PaymentType/usePromptpay.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+export default function usePromptpay(amount, paymentType) {
+  const [paymentInfo, setPaymentInfo] = useState("");
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleScriptLoad = () => {
+    setScriptLoaded(true);
+  };
+
+  const handleFormSubmit = () => {
+    setIsLoading(true);
+    const paymentData = {
+      amount: amount, // Example amount in satangs
+      currency: "THB",
+    };
+
+    if (window.Omise) {
+      window.Omise.createSource(
+        "promptpay",
+        paymentData,
+        (statusCode, response) => {
+          if (response.object === "source" && response.id) {
+            sendSourceToBackend(response.id, response.amount).finally(() => {
+              setIsLoading(false);
+            });
+            console.log(response);
+          } else {
+            console.error(response);
+          }
+        }
+      );
+    } else {
+      console.error("Omise library not loaded");
+    }
+  };
+  const sendSourceToBackend = async (sourceId, amount) => {
+    try {
+      const response = await fetch("/api/payment/promptpay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ sourceId, amount }), // pass sourceId an attribute
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      setPaymentInfo(json.data); //json.imageObject.download_uri
+
+      // console.log("Charge created:", data);
+    } catch (error) {
+      console.error("Error sending source to backend:", error);
+    }
+  };
+  return {
+    scriptLoaded,
+    paymentInfo,
+    isLoading,
+    handleFormSubmit,
+    handleScriptLoad,
+    sendSourceToBackend,
+  };
+}
